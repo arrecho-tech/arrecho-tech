276a942bfe070c973165e290c0388a20f610ddbe
d3qeruno <d3qeruno@arrecho.tech>
2026-02-26 17:48:04 -0700

    Simplify homepage

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,7 +2,6 @@ import { headers as getHeaders } from 'next/headers.js'
 import Image from 'next/image'
 import { getPayload } from 'payload'
 import React from 'react'
-import { fileURLToPath } from 'url'
 
 import config from '@/payload.config'
 import './styles.css'
@@ -13,36 +12,20 @@ export default async function HomePage() {
   const payload = await getPayload({ config: payloadConfig })
   const { user } = await payload.auth({ headers })
 
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
-
   return (
     <div className="home">
       <div className="content">
         <picture>
           <source srcSet="/brand/logo-circle.svg" />
-          <Image alt="Arrecho Tech" height={65} src="/brand/logo-circle.svg" width={65} />
+          <Image
+            alt="Arrecho Tech"
+            height={65}
+            src="/brand/logo-circle.svg"
+            width={65}
+          />
         </picture>
         {!user && <h1>Welcome to Arrecho Tech.</h1>}
         {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Admin
-          </a>
-          <a className="docs" href="https://payloadcms.com/docs" rel="noopener noreferrer" target="_blank">
-            Payload Docs
-          </a>
-        </div>
-      </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
       </div>
     </div>
   )
